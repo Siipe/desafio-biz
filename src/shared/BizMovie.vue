cb37a34765135ef37b4789985af199ed9dbf3270
Siipe <siipecapoeira@gmail.com>
2020-02-12 00:23:49 -0300

    Adding modal and popovers

--- src/shared/BizMovie.vue
+++ src/shared/BizMovie.vue
@@ -2,11 +2,14 @@
   <b-col xl="2" lg="3" md="4" sm="5">
     <div class="biz-movie-container mb-3" :style="`width: ${defaultWidth}px`">
       <button
+        :id="`watchlist-star${movie.id}`"
         @click="handleFavorite"
-        class="biz-btn-fav"
-        title="Add to the watchlist">
+        class="biz-btn-fav">
         <b-icon icon="star-fill" :variant="iconVariant" />
       </button>
+      <b-popover :target="`watchlist-star${movie.id}`" triggers="hover" placement="top">
+        {{ movie.favorite ? 'Remove from' : 'Add to' }} the <strong> watchlist</strong>
+      </b-popover>
       <b-img
         :src="src"
         :alt="movie.title"
@@ -14,6 +17,61 @@
         class="biz-movie-img"
         thumbnail />
     </div>
+
+    <b-modal
+      v-if="visible"
+      v-model="visible"
+      size="lg"
+      centered
+      button-size="sm"
+      no-fade
+      hide-header
+      hide-footer
+      body-class="biz-modal-body">
+      <b-card no-body class="overflow-hidden">
+        <b-row no-gutters>
+          <b-col md="6">
+            <b-card-img :src="src" class="rounded-0" />
+          </b-col>
+          <b-col md="6">
+            <b-card-header>
+              <b-icon
+                style="cursor: pointer; position: absolute; top: 4px; right: 4px"
+                icon="x"
+                variant="secondary"
+                font-scale="1.4"
+                @click="visible = false" />
+              <h3>{{ movie.title }} <small class="text-secondary">({{ movie.getFormattedDate('YYYY') }})</small></h3>
+              <b-row>
+                <b-col cols="3">
+                  <b-icon icon="star-fill" variant="fav-star-checked" font-scale="1.4" class="mr-1" />
+                  <small>{{ movie.vote_average }}</small>
+                </b-col>
+                <b-col cols="9">
+                  <b-icon icon="calendar" variant="secondary" font-scale="1.2" class="mr-1" />
+                  <small>{{ movie.getFormattedDate('LL') }}</small>
+                </b-col>
+              </b-row>
+              <b-row>
+                <b-col cols="12">
+                </b-col>
+              </b-row>
+            </b-card-header>
+            <b-card-body>
+              <b-card-text>
+                <div v-if="movie.original_title">
+                  <small><strong>Original title: </strong>{{ movie.original_title }}</small>
+                  <hr />
+                </div>
+                <p v-if="movie.overview" class="text-justify">
+                  <strong>Overview: </strong>{{ movie.overview }}
+                </p>
+              </b-card-text>
+            </b-card-body>
+          </b-col>
+        </b-row>
+      </b-card>
+    </b-modal>
   </b-col>
 </template>
 <script>
@@ -30,13 +88,15 @@ export default {
   data() {
     return {
       defaultWidth: 154,
+      visible: false,
     };
   },
   computed: {
     src() {
+      const imgWidth = this.visible ? 500 : this.defaultWidth;
       return this.movie.hasPoster
-        ? `https://image.tmdb.org/t/p/${this.movie.getImagePathWithSize(`w${this.defaultWidth}`)}`
-        : 'https://dummyimage.com/300x200/#CCC/fff.jpg&text=no_image';
+        ? `https://image.tmdb.org/t/p/${this.movie.getImagePathWithSize(`w${imgWidth}`)}`
+        : `https://picsum.photos/${imgWidth}/${imgWidth}/?image=20`;
     },
     iconVariant() {
       return this.movie.favorite
@@ -50,7 +110,7 @@ export default {
       this.$emit(event, this.movie);
     },
     handleSelected() {
-      this.$emit('bizmovieSelected', this.movie);
+      this.visible = true;
     }
   },
 };
