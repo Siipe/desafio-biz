885f41ee6e2825e7b92dac0745ecb5b1fe3c8ff2
Siipe <siipecapoeira@gmail.com>
2020-02-14 01:23:17 -0300

    Lib nprogress and using environment variables

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+import 'nprogress/nprogress.css';
 import './assets/scss/custom.scss';
 import Vue from 'vue';
 import VueRouter from 'vue-router';
--- src/movie/service.js
+++ src/movie/service.js
@@ -1,17 +1,31 @@
 import axios from 'axios';
+import nprogress from 'nprogress';
 import Movie from './movie';
 
-const baseURL = 'https://api.themoviedb.org/3/';
-const apiKey = '712d3949e81088a58139e11a6bf5c14f';
+const baseURL = process.env.VUE_APP_BASE_URL;
+const apiKey = process.env.VUE_APP_API_KEY;
 
 const instance = axios.create({ baseURL });
 
 instance.interceptors.request.use((config) => {
+  nprogress.start();
   const params = config.params || new URLSearchParams();
   params.set('api_key', apiKey);
   config.params = params;
   return config;
-}, (error) => Promise.reject(error));
+}, (error) => {
+  nprogress.done();
+  return Promise.reject(error);
+});
+
+instance.interceptors.response.use((config) => {
+  nprogress.done();
+  return config;
+}, (error) => {
+  nprogress.done();
+  return Promise.reject(error);
+});
+
 
 /**
  * Converts each retrieved movie to an instance of the Movie class
